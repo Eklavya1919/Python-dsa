ac1e589347b53b8d8cce6ebd6adb62ff6d47f3de
Eklavya <eklavyashridharshinde@gmail.com>
2026-04-21 22:41:05 +0530

    revise

--- 9.2 bubble sort.py
+++ 9.2 bubble sort.py
@@ -1,12 +1,28 @@
 nums = [5,4,3,1,2]
 
+# while True:
+#     swapped = False
+#     for i in range(len(nums)-1):
+#         if nums[i]>nums[i+1]:
+#             nums[i],nums[i+1]=nums[i+1],nums[i]
+#             swapped = True
+#     if swapped==False:
+#         break
+
+# print(nums)
+
+
+
+
+# revision
 while True:
     swapped = False
     for i in range(len(nums)-1):
         if nums[i]>nums[i+1]:
-            nums[i],nums[i+1]=nums[i+1],nums[i]
+            nums[i], nums[i+1]= nums[i+1], nums[i]
             swapped = True
     if swapped==False:
         break
 
 print(nums)
+        
